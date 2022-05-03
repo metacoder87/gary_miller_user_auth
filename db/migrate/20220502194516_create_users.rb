3f8b7ff2ab2c4cbf94dc9009dc6f14f179ab3678
metacoder87 <gary.miller@gmx.com>
2022-05-03 01:20:09 -0400

    Fixed typo in users table

--- db/migrate/20220502194516_create_users.rb
+++ db/migrate/20220502194516_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username, null: false
       t.string :session_token, null: false
-      t.string :passwor_digest, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
     end
