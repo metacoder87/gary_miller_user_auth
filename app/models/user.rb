27b619ccdfdeebd200a506dc22d407bcefcfacc9
metacoder87 <gary.miller@gmx.com>
2022-05-03 00:56:48 -0400

    Adds ::find by credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,3 +5,11 @@ class User < ApplicationRecord
     before_validation :ensure_session_token
 
     attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+        nil
+    end
+
+end
